a369454cfed46b5cd4fe0609e5caa0152703543a
Rob Weaver <rob@accuweaver.com>
2015-05-23 10:09:12 -0600

    Fixed unit PHPUnit tests for task 001.

--- tasks/001/test.php
+++ tasks/001/test.php
@@ -9,12 +9,17 @@ class Test extends PHPUnit_Framework_TestCase {
   //please fix the tests below so that they pass
   //you should only need to fix the value under test (ie the second paramter to the assert* methods)
   public function testBasicKnowledge(){
-    $this->assertEquals(3*15, false);
-    $this->assertEquals("boo" . " " . "yah", false);
+    $this->assertEquals(3*15, 45);
+    $this->assertEquals("boo" . " " . "yah", "boo yah");
     $arr = array();
     array_push($arr, "foo");
     array_push($arr, "bar");
     array_unshift($arr, "baz");
-    $this->assertEquals($arr, false);
+    $expected = array(
+        0 => "baz",
+        1 => "foo",
+        2 => "bar"
+    );
+    $this->assertEquals($arr, $expected);
   }
 }
